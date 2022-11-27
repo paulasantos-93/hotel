66c3c10ffa11e7d373934721c0665427f26e0ff7
Paula <paulasantos_risa@hotmail.com>
2022-11-27 16:55:22 +0000

    last implementations

---
+++ src/org/academiadecodigo/bootcamp/Room.java
@@ -0,0 +1,21 @@
+package org.academiadecodigo.bootcamp;
+
+/**
+ * Created by ferrao on 29/01/16.
+ */
+public class Room {
+
+    private boolean available = true;
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void occupy() {
+        available = false;
+    }
+
+    public void vacate() {
+        available = true;
+    }
+}
